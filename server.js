3a11f1f8ac95f18f7809fa1bd279557a3c848beb
Hazera Munim <hazeramunim@Hazeras-MacBook-Air.local>
2020-02-16 22:43:38 -0500

    created characters objects

--- server.js
+++ server.js
@@ -10,3 +10,27 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Star Wars Characters (DATA)
+var characters = [{
+    routeName: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+},
+{
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+},
+{
+    routeName: "obiwankenobi",
+    name: "Obi Wan Kenobi",
+    role: "Jedi Master",
+    age: 55,
+    forcePoints: 1350
+}
+];
