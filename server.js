bf6432e0479129acfdde0902f7bbf30df5e60691
Hazera Munim <hazeramunim@Hazeras-MacBook-Air.local>
2020-02-17 21:43:51 -0500

    basic routes

--- server.js
+++ server.js
@@ -35,3 +35,12 @@ var characters = [{
 }
 ];
 
+// Routes
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+});
